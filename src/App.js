5a13500bf2d49384305b4b8e5fb896a726f2f63a
Yenni Vanessa Delgado <yennivanessadelgado@gmail.com>
2023-04-12 23:33:03 -0500

    style: add styles to superHeroCard

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React, { useEffect } from "react";
 import SearchHero from "./components/SearchHero";
 import { connect } from "react-redux";
 import { useDispatch } from "react-redux";
+// import FilterHero from "./components/FilterHero;
 
 function App(props) {
   return (
     <div className="App">
       <header>
         <SearchHero />
+        {/* <FilterHero/> */}
       </header>
       <h1> Super Hero App</h1>
       {props.loadingState ? <h1>loading....</h1> : <></>}
